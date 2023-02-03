3f288aefde7ff39c2086332fdd070fcae1b55bbc
hk512 <58211853+KentaroHanda@users.noreply.github.com>
2023-02-03 23:03:26 +0900

    Slackへの通知処理

---
+++ controller/notificator.py
@@ -0,0 +1,12 @@
+import json
+
+import requests
+
+
+class Notificator(object):
+    def __init__(self, webhook_url):
+        self.webhook_url = webhook_url
+
+    def notify(self, payload):
+        res = requests.post(url=self.webhook_url, data=json.dumps(payload))
+        return res
