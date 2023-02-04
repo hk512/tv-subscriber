e0009ceaf186aefe6652b1ceb81621e1316bf225
hk512 <58211853+KentaroHanda@users.noreply.github.com>
2023-02-04 16:54:18 +0900

    TradingViewのwebhook受け取りサーバーの追加

---
+++ server/server.py
@@ -0,0 +1,64 @@
+import ipaddress
+
+from flask import abort
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from controller.trader import Trader
+from config import config
+
+app = Flask(__name__)
+
+# allowed ip address
+ALLOW_NETWORKS = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
+
+
+@app.before_request
+def before_request():
+    remote_address = ipaddress.ip_address(request.remote_addr)
+    for allow_network in ALLOW_NETWORKS:
+        ip_network = ipaddress.ip_network(allow_network)
+        if remote_address in ip_network:
+            return
+
+    return abort(403, "access denied your IP address")
+
+
+@app.route("/", methods=["POST"])
+def run():
+    signal = request.data.decode()
+    trader = Trader(
+        api_key=config.api_key,
+        api_secret=config.api_secret,
+        symbol=config.symbol,
+        lot=config.lot,
+        max_lot=config.max_lot,
+    )
+
+    if signal == "Buy":
+        close_position_result = trader.close_position("sell")
+
+        if not close_position_result:
+            return jsonify({"result": -1}), 400
+
+        create_position_result = trader.create_position("buy")
+
+        if not create_position_result:
+            return jsonify({"result": -1}), 400
+
+    elif signal == "Sell":
+        close_position_result = trader.close_position("buy")
+
+        if not close_position_result:
+            return jsonify({"result": -1}), 400
+
+        create_position_result = trader.create_position("sell")
+
+        if not create_position_result:
+            return jsonify({"result": -1}), 400
+
+    else:
+        return jsonify({"result": -1}), 400
+
+    return jsonify({"result": 0}), 200
