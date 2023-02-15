e087493d9a349be61f08202647964389fa3fac6b
kentaro.handa <kentaro.handa@gmail.com>
2023-02-15 22:47:51 +0900

    取引に失敗した時にSlackに通知する処理を追加

--- controller/notificator.py
+++ controller/notificator.py
@@ -7,6 +7,14 @@ class Notificator(object):
     def __init__(self, webhook_url):
         self.webhook_url = webhook_url
 
-    def notify(self, payload):
+    def notify(self, text):
+        payload = {
+            "username": f"tv-subscriber",
+            "attachments": [
+                {
+                    "text": text,
+                }
+            ],
+        }
         res = requests.post(url=self.webhook_url, data=json.dumps(payload))
         return res
--- server/server.py
+++ server/server.py
@@ -7,6 +7,7 @@ from flask import jsonify
 from flask import request
 
 from controller.trader import Trader
+from controller.notificator import Notificator
 from config import config
 
 app = Flask(__name__)
@@ -41,27 +42,37 @@ def run():
         max_lot=config.max_lot,
     )
 
+    notificator = Notificator(config.webhook_url)
+
     if signal == "Buy":
         close_position_result = trader.close_position("sell")
 
         if not close_position_result:
-            return jsonify({"result": -1}), 400
+            msg = "Failed to close the sell position."
+            notificator.notify(msg)
+            return jsonify({"result": -1, "msg": msg}), 400
 
         create_position_result = trader.create_position("buy")
 
         if not create_position_result:
-            return jsonify({"result": -1}), 400
+            msg = "Failed to create the sell position."
+            notificator.notify(msg)
+            return jsonify({"result": -1, "message": msg}), 400
 
     elif signal == "Sell":
         close_position_result = trader.close_position("buy")
 
         if not close_position_result:
-            return jsonify({"result": -1}), 400
+            msg = "Failed to close the buy position."
+            notificator.notify(msg)
+            return jsonify({"result": -1, "msg": msg}), 400
 
         create_position_result = trader.create_position("sell")
 
         if not create_position_result:
-            return jsonify({"result": -1}), 400
+            msg = "Failed to create the sell position."
+            notificator.notify(msg)
+            return jsonify({"result": -1, "msg": msg}), 400
 
     else:
         return jsonify({"result": -1}), 400
